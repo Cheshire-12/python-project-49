ef2a1aab2c4b7b0bdc80fa706e76f0dd500a08c0
Andrey <andr.vnk@gmail.com>
2025-10-29 20:28:40 +0300

    The variable names have been corrected

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,14 +7,14 @@ DESCRIPTION = 'What is the result of the expression?'
 
 # Question and correct answer generator for Calc game.
 def generate_question():
-    ops = {'+': operator.add,
+    operations = {'+': operator.add,
                  '-': operator.sub,
                  '*': operator.mul
                  }
-    ops_symbols = random.choice(list(ops.keys()))
+    op_symbol = random.choice(list(operations.keys()))
     num1 = random.randint(1, 25)
     num2 = random.randint(1, 25)
-    operator_func = ops[ops_symbols]
-    question = f"{num1} {ops_symbols} {num2}"
+    operator_func = operations[op_symbol]
+    question = f"{num1} {op_symbol} {num2}"
     correct_answer = operator_func(num1, num2)
     return question, correct_answer
