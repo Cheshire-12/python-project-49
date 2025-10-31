12821989febb838a3e7a75487a344330e7eab0a2
Andrey <andr.vnk@gmail.com>
2025-10-31 16:56:16 +0300

    added #NOSONAR

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,7 +8,7 @@ def question_generator():
     progression_length = random.randint(5, 10) #NOSONAR
     start = random.randint(1, 20) #NOSONAR
     step = random.randint(1, 5) #NOSONAR
-    hidden_index = random.randint(1, progression_length - 2)
+    hidden_index = random.randint(1, progression_length - 2) #NOSONAR
     progression = [str(start + i * step) for i in range(progression_length)]
     correct_answer = progression[hidden_index]
     progression[hidden_index] = '..'
