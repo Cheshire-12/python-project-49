f545af1365a766cced233497f45dda919bc398f6
Andrey <andr.vnk@gmail.com>
2025-10-31 16:38:38 +0300

    added prime prime game

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,23 @@
+import random
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+# We check whether it is a prime number or not.
+def is_prime_number(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# Question and correct answer generator for Prime game.
+def generate_question():
+    num = random.randint(1, 100)
+    question = str(num)
+    correct_answer = 'yes' if is_prime_number(num) else 'no'
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.engine import game_engine
+from brain_games.games.brain_prime import DESCRIPTION, generate_question
+
+
+def main():
+    game_engine(DESCRIPTION, generate_question)
+    
+    
+if __name__ == '__main__':
+    main()
